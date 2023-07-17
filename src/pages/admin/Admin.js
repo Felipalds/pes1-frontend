fc8bdfdf5890232f76f6541a64d71a744e7fbbcd
Luiz Felipe Fonseca Rosa <luizfelipefonsecarosa@gmail.com>
2023-07-17 15:09:38 -0300

    a

--- src/pages/admin/Admin.js
+++ src/pages/admin/Admin.js
@@ -5,6 +5,8 @@ import {FieldArray, Form, Formik} from "formik"
 export const Admin = () => {
 
     const [ professionals, setProfessionals ] = React.useState([])
+    const [ procedures, setProcedures ] = React.useState([])
+
 
     React.useEffect(() => {
         if(professionals.length === 0)
@@ -13,20 +15,46 @@ export const Admin = () => {
         .then(res => {
             setProfessionals(res.data)
         })
+
+        if(procedures.length === 0)
+        axios
+        .get("http://localhost:3001/procedures")
+        .then(res => {
+            setProcedures(res.data)
+        })
         }
+        
     )
 
-    const onSubmit = () => {
+
+    const handleAdd = () => {
+
+        const name = document.getElementById("addName").value
+        const description = document.getElementById("addDescription").value
+        const slug = document.getElementById("addSlug").value
+
+        axios.post("http://localhost:3001/professionals", {name, description, slug})
+
+        setProfessionals(professionals => [...professionals, { name,  description, slug }])
+        document.getElementById("addName").value = ""
+        document.getElementById("addDescription").value = ""
+        document.getElementById("addSlug").value = ""
 
     }
 
-    const handleAdd = () => {
-        const name = document.getElementById("addName").value
-        const description = document.getElementById("addDescription").value
+    const handleAddProcedure = () => {
 
-        setProfessionals(professionals => [...professionals, { name,  description }])
-        document.getElementById("addName").value = ""
-        document.getElementById("addDescription").value = ""
+        const name = document.getElementById("addProcedureName").value
+        const description = document.getElementById("addProcedureDescription").value
+        const slug = document.getElementById("addProcedureSlug").value
+
+        axios.post("http://localhost:3001/procedures", {name, description, slug})
+
+        setProcedures(procedures => [...procedures, { name,  description, slug }])
+        document.getElementById("addProcedureName").value = ""
+        document.getElementById("addProcedureDescription").value = ""
+        document.getElementById("addProcedureSlug").value = ""
+
     }
 
     return (
@@ -35,22 +63,37 @@ export const Admin = () => {
             <h2>Formulário de profissionais</h2>
             <form onSubmit={e => {e.preventDefault()}} >
                 {professionals.map(p => {
-                    return(<div style={{"paddingBottom" : "100px"}}>
-                        nome:<input defaultValue={p.name} placeholder="Nome" type="text"></input>
-                        descricao:<input defaultValue={p.description} placeholder="Descrição" type="text"></input>
+                    return(<div style={{"paddingBottom" : "50px"}}>
+                        nome:<input defaultValue={p.name} placeholder="Nome" type="text" disabled={true}></input>
+                        descricao:<input defaultValue={p.description} placeholder="Descrição" type="text" disabled={true}></input>
+                        slug:<input defaultValue={p.description} placeholder="Slug" type="text" disabled={true}></input>
                     </div>)
                 })}
                 <hr />
                 <input placeholder="Nome" type="text" id="addName"></input>
                 <input placeholder="Descrição" type="text" id="addDescription"></input>
-                <button onClick={() => handleAdd()}> Adicionar </button>
-                <button onClick={() => {}}> Salvar </button>
+                <input placeholder="Slug" type="text" id="addSlug"></input>
 
+                <button onClick={() => handleAdd()}> Adicionar </button>
             </form>
 
 
             <h2>Formulário de procedimentos</h2>
+            <form onSubmit={e => {e.preventDefault()}} >
+                {procedures.map(p => {
+                    return(<div style={{"paddingBottom" : "50px"}}>
+                        nome:<input defaultValue={p.name} placeholder="Nome" type="text" disabled={true}></input>
+                        descricao:<input defaultValue={p.description} placeholder="Descrição" type="text" disabled={true}></input>
+                        slug:<input defaultValue={p.slug} placeholder="Slug" type="text" disabled={true}></input>
+                    </div>)
+                })}
+                <hr />
+                <input placeholder="Nome" type="text" id="addProcedureName"></input>
+                <input placeholder="Descrição" type="text" id="addProcedureDescription"></input>
+                <input placeholder="Slug" type="text" id="addProcedureSlug"></input>
 
+                <button onClick={() => handleAddProcedure()}> Adicionar </button>
+            </form>
 
 
         </div>
